a97f6b68e668ce4310ae8e476e7447c178c1fc49
takna <tak_na@icloud.com>
2023-08-20 17:21:06 +0900

    Add: Implement SoundEnabledProvider Component.

--- src/components/SoundEnabledProvider/SoundEnabledProvider.js
+++ src/components/SoundEnabledProvider/SoundEnabledProvider.js
@@ -1,7 +1,31 @@
+'use client';
 import React from 'react';
 
-function SoundEnabledProvider() {
-  return <div></div>;
+const soundEnabledContext = React.createContext();
+
+function SoundEnabledProvider({ children }) {
+  const [soundEnabled, setSoundEnabled] =
+    React.useState(true);
+
+  return (
+    <soundEnabledContext.Provider
+      value={{ soundEnabled, setSoundEnabled }}
+    >
+      {children}
+    </soundEnabledContext.Provider>
+  );
+}
+
+export function useSoundEnabled() {
+  const data = React.useContext(soundEnabledContext);
+
+  if (!data) {
+    throw new Error(
+      'Cannot consume SoundEnabled context without SoundEnabledProvider'
+    );
+  }
+
+  return data;
 }
 
 export default SoundEnabledProvider;
